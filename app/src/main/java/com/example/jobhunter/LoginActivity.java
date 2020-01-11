56029c61828b18197782b8fedbbdc508499b6dc2
ezhilnero <ezhil99mutsun@gmail.com>
2020-01-11 16:32:56 +0530

    login page UI completed - ezhil

--- app/src/main/java/com/example/jobhunter/LoginActivity.java
+++ app/src/main/java/com/example/jobhunter/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.jobhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
 
         //Intialization of UI
         initUI();
