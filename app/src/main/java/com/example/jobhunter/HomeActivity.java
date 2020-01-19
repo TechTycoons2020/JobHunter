dd627125121a01d1610eb2add14a9124c5707f99
ezhilnero <ezhil99mutsun@gmail.com>
2020-01-19 21:32:05 +0530

    job details page done for GOVT and Intern - ezhil

--- app/src/main/java/com/example/jobhunter/HomeActivity.java
+++ app/src/main/java/com/example/jobhunter/HomeActivity.java
@@ -71,7 +71,7 @@ public class HomeActivity extends AppCompatActivity {
         settingsIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),ResumeActivity.class);
+                Intent intent=new Intent(getApplicationContext(),SettingActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/jobhunter/SettingActivity.java
+++ app/src/main/java/com/example/jobhunter/SettingActivity.java
@@ -2,13 +2,35 @@ package com.example.jobhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
+    TextView profile , resume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        initUI();
+
+        resume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ResumeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private void initUI() {
+        profile = findViewById(R.id.profileTV);
+        resume = findViewById(R.id.resumeTV);
     }
 }
